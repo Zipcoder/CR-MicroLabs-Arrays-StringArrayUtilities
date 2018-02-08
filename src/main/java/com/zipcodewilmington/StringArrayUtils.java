1b47f8f3b85cb5288bea22f918ad346fa99b785f
Jessica Campbell <jessicacampbell@zipcoders-MacBook-Pro-42.local>
2018-02-08 18:15:07 -0500

    pangram power

--- src/main/java/com/zipcodewilmington/StringArrayUtils.java
+++ src/main/java/com/zipcodewilmington/StringArrayUtils.java
@@ -1,4 +1,5 @@
 package com.zipcodewilmington;
+import java.util.Arrays;
 
 /**
  * Created by leon on 1/29/18.
@@ -9,15 +10,18 @@ public class StringArrayUtils {
      * @return first element of specified array
      */ // TODO
     public static String getFirstElement(String[] array) {
-        return null;
+      //  System.out.println("First Element");
+        return array[0];
     }
+
 
     /**
      * @param array array of String objects
      * @return second element in specified array
      */
     public static String getSecondElement(String[] array) {
-        return null;
+      //  System.out.println("Second Element");
+        return array[1];
     }
 
     /**
@@ -25,7 +29,8 @@ public class StringArrayUtils {
      * @return last element in specified array
      */ // TODO
     public static String getLastElement(String[] array) {
-        return null;
+       // System.out.println("Last Element");
+        return array[array.length - 1];
     }
 
     /**
@@ -33,7 +38,8 @@ public class StringArrayUtils {
      * @return second to last element in specified array
      */ // TODO
     public static String getSecondToLastElement(String[] array) {
-        return null;
+       // System.out.println("Second to Last");
+        return array[array.length - 2];
     }
 
     /**
@@ -42,15 +48,32 @@ public class StringArrayUtils {
      * @return true if the array contains the specified `value`
      */ // TODO
     public static boolean contains(String[] array, String value) {
-        return false;
-    }
+        //System.out.println("Boolean Array!");
+        Boolean matchFound = false;
+        for (int i = 0; i < array.length; i++){
+                if (array[i].equals(value)) {
+                    matchFound = true;
+                }
+           }
+        return matchFound;
+        }
 
     /**
      * @param array of String objects
      * @return an array with identical contents in reverse order
      */ // TODO
     public static String[] reverse(String[] array) {
-        return null;
+
+        String[] reverse = new String[array.length];
+
+        int newStringNum = 0;
+
+        for (int i = array.length - 1; i >= 0 ; i--) {
+
+            reverse[newStringNum] = array[i];
+            newStringNum++;
+        }
+        return reverse;
     }
 
     /**
@@ -58,15 +81,27 @@ public class StringArrayUtils {
      * @return true if the order of the array is the same backwards and forwards
      */ // TODO
     public static boolean isPalindromic(String[] array) {
-        return false;
-    }
+        return Arrays.equals(reverse(array), array);
+        }
+
+    // calling reverse function from previous w a parameter of current array and compare to current array in this function
 
     /**
      * @param array array of String objects
      * @return true if each letter in the alphabet has been used in the array
      */ // TODO
     public static boolean isPangramic(String[] array) {
-        return false;
+        // when putting in your own information use curly braces
+        String alphabet = "";
+        Boolean values = false;
+
+        for (char ch = 'a'; i <= 'z'; ch++) {
+            alphabet += 1;
+            if (array.indexOf(ch) < 0){
+                values = true;
+            }
+        }
+        return values;
     }
 
     /**
