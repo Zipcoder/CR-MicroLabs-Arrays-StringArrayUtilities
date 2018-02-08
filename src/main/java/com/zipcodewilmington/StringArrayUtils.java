40d886d6a201dcc0d3c8b1ac21965de22f6460d1
Eric Barnaba <ericbarnaba@zipcoders-MacBook-Pro-25.local>
2018-02-08 14:26:21 -0500

    first commit

--- src/main/java/com/zipcodewilmington/StringArrayUtils.java
+++ src/main/java/com/zipcodewilmington/StringArrayUtils.java
@@ -9,7 +9,8 @@ public class StringArrayUtils {
      * @return first element of specified array
      */ // TODO
     public static String getFirstElement(String[] array) {
-        return null;
+
+        return array[0];
     }
 
     /**
@@ -17,7 +18,8 @@ public class StringArrayUtils {
      * @return second element in specified array
      */
     public static String getSecondElement(String[] array) {
-        return null;
+
+        return array [1] ;
     }
 
     /**
@@ -25,7 +27,8 @@ public class StringArrayUtils {
      * @return last element in specified array
      */ // TODO
     public static String getLastElement(String[] array) {
-        return null;
+
+        return array [array.length-1];
     }
 
     /**
@@ -33,7 +36,8 @@ public class StringArrayUtils {
      * @return second to last element in specified array
      */ // TODO
     public static String getSecondToLastElement(String[] array) {
-        return null;
+
+        return array [array.length-2];
     }
 
     /**
@@ -42,7 +46,14 @@ public class StringArrayUtils {
      * @return true if the array contains the specified `value`
      */ // TODO
     public static boolean contains(String[] array, String value) {
-        return false;
+        boolean answer = false;
+
+        for (String s : array) {
+             if(s.equals(value)) {
+                 answer = true;
+             }
+        }
+        return answer;
     }
 
     /**
@@ -50,7 +61,14 @@ public class StringArrayUtils {
      * @return an array with identical contents in reverse order
      */ // TODO
     public static String[] reverse(String[] array) {
-        return null;
+        String[] reverse = new String[array.length];
+        int index = 0;
+        for (int i = array.length-1; i>=0; i--){
+            reverse[index] = array[i];
+            index++;
+        }
+
+        return reverse;
     }
 
     /**
