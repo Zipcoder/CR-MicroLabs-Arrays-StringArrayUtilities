c2e2e136fcac17460f0c95e27b8dcf487cdcd591
Kaitrina High <kaitrinahigh@zipcoders-MacBook-Pro-81.local>
2018-02-09 21:15:11 -0500

    Kats Array Util

--- src/main/java/com/zipcodewilmington/StringArrayUtils.java
+++ src/main/java/com/zipcodewilmington/StringArrayUtils.java
@@ -1,4 +1,5 @@
 package com.zipcodewilmington;
+import java.util.Arrays;
 
 /**
  * Created by leon on 1/29/18.
@@ -9,7 +10,8 @@ public class StringArrayUtils {
      * @return first element of specified array
      */ // TODO
     public static String getFirstElement(String[] array) {
-        return null;
+
+        return array[0];
     }
 
     /**
@@ -17,7 +19,8 @@ public class StringArrayUtils {
      * @return second element in specified array
      */
     public static String getSecondElement(String[] array) {
-        return null;
+
+        return array[1];
     }
 
     /**
@@ -25,7 +28,8 @@ public class StringArrayUtils {
      * @return last element in specified array
      */ // TODO
     public static String getLastElement(String[] array) {
-        return null;
+
+        return array[array.length - 1];
     }
 
     /**
@@ -33,7 +37,8 @@ public class StringArrayUtils {
      * @return second to last element in specified array
      */ // TODO
     public static String getSecondToLastElement(String[] array) {
-        return null;
+
+        return array[array.length - 2];
     }
 
     /**
@@ -42,6 +47,11 @@ public class StringArrayUtils {
      * @return true if the array contains the specified `value`
      */ // TODO
     public static boolean contains(String[] array, String value) {
+
+        for (String myString : array) {
+            if (myString.equals(value))
+                return true;
+        }
         return false;
     }
 
@@ -50,7 +60,14 @@ public class StringArrayUtils {
      * @return an array with identical contents in reverse order
      */ // TODO
     public static String[] reverse(String[] array) {
-        return null;
+
+        String[] result = new String[array.length];
+        int count = 0;
+        for (int i = array.length - 1; i >= 0; i--) {
+            result[count] = array[i];
+            count += 1;
+        }
+        return result;
     }
 
     /**
@@ -58,7 +75,9 @@ public class StringArrayUtils {
      * @return true if the order of the array is the same backwards and forwards
      */ // TODO
     public static boolean isPalindromic(String[] array) {
-        return false;
+
+        return Arrays.equals(array, reverse(array));
+
     }
 
     /**
@@ -66,8 +85,28 @@ public class StringArrayUtils {
      * @return true if each letter in the alphabet has been used in the array
      */ // TODO
     public static boolean isPangramic(String[] array) {
-        return false;
+
+        boolean[] switches = new boolean[26];
+
+        for (int i = 0; i < array.length; i++) {
+            String elementOfArray = array[i];
+            elementOfArray = elementOfArray.toUpperCase();
+            for (int j = 0; j < elementOfArray.length(); j++) {
+                int ascii = elementOfArray.charAt(j) - 65;
+                if (ascii >= 0 && ascii < 26) {
+                    switches[ascii] = true;
+                }
+            }
+        }
+
+        for (int k = 0; k < 26; k++) {
+            if (!switches[k]) {
+                return false;
+            }
+        }
+        return true;
     }
+
 
     /**
      * @param array array of String objects
@@ -75,7 +114,14 @@ public class StringArrayUtils {
      * @return number of occurrences the specified `value` has occurred
      */ // TODO
     public static int getNumberOfOccurrences(String[] array, String value) {
-        return 0;
+
+        int valOccurs = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].equals(value)) {
+                valOccurs++;
+            }
+        }
+        return valOccurs;
     }
 
     /**
@@ -84,7 +130,20 @@ public class StringArrayUtils {
      * @return array with identical contents excluding values of `value`
      */ // TODO
     public static String[] removeValue(String[] array, String valueToRemove) {
-        return null;
+
+        int newArraySize = array.length - getNumberOfOccurrences(array, valueToRemove);
+        String[] resultArray = new String[newArraySize];
+        int shift = 0;
+
+        for (int i = 0; i < array.length; i++) {
+
+            if (array[i].equals(valueToRemove)) {
+                shift++;
+            } else {
+                resultArray[i - shift] = array[i];
+            }
+        }
+        return resultArray;
     }
 
     /**
@@ -92,7 +151,28 @@ public class StringArrayUtils {
      * @return array of Strings with consecutive duplicates removes
      */ // TODO
     public static String[] removeConsecutiveDuplicates(String[] array) {
-        return null;
+        int newArraySize = array.length - duplicatesCount(array);
+        String[] resultArray = new String[newArraySize];
+        int resultArrayIndex = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (i == 0 || !array[i].equals(array[i - 1])) {
+                resultArray[resultArrayIndex] = array[i];
+                resultArrayIndex++;
+            }
+        }
+        return resultArray;
+    }
+
+    public static int duplicatesCount(String[] array) {
+
+        int dupCount = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i].equals(array[i - 1])) {
+                dupCount++;
+            }
+        }
+        return dupCount;
     }
 
     /**
@@ -100,8 +180,24 @@ public class StringArrayUtils {
      * @return array of Strings with each consecutive duplicate occurrence concatenated as a single string in an array of Strings
      */ // TODO
     public static String[] packConsecutiveDuplicates(String[] array) {
-        return null;
+
+        int newArraySize = array.length - duplicatesCount(array);
+        String[] resultArray = new String[newArraySize];
+        int resultArrayIndex = 0;
+        String concatStr = array[0];
+        String currStr = array[0];
+
+        for (int i = 1; i < array.length; i++) {
+            if(array[i].equals(currStr)){
+                concatStr += currStr;
+            }else{
+                resultArray[resultArrayIndex] = concatStr;
+                resultArrayIndex++;
+                concatStr = array[i];
+                currStr = array[i];
+            }
+        }
+        resultArray[resultArrayIndex] = concatStr;
+        return resultArray;
     }
-
-
 }
--- src/test/java/com/zipcodewilmington/StringArrayUtilsTest.java
+++ src/test/java/com/zipcodewilmington/StringArrayUtilsTest.java
@@ -233,7 +233,7 @@ public class StringArrayUtilsTest {
 
     @Test
     public void testIsPalindromic2() {
-        String[] array = {"Is this a plaindrome?", "This is a plaindrome", "Is this a palindrome?"};
+        String[] array = {"Is this a palindrome?", "This is a palindrome", "Is this a palindrome?"};
         boolean outcome = StringArrayUtils.isPalindromic(array);
         Assert.assertTrue(outcome);
     }
@@ -241,9 +241,9 @@ public class StringArrayUtilsTest {
 
     @Test
     public void testIsPalindromic3() {
-        String[] array = {"Is this a plaindrome?", "This is not a plaindrome", "Is this a palindrome?", "This is not a palindrome"};
+        String[] array = {"Is this a palindrome", "This is not a palindrome", "Is this a palindrome", "This is not a palindrome"};
         boolean outcome = StringArrayUtils.isPalindromic(array);
-        Assert.assertTrue(outcome);
+        Assert.assertFalse(outcome);
     }
 
 
@@ -323,6 +323,13 @@ public class StringArrayUtilsTest {
 
 
 
+    @Test
+    public void testRemoveConsecutiveDuplicates11() {
+        String[] array = {"aba", "aba"};
+        String[] actual = StringArrayUtils.removeConsecutiveDuplicates(array);
+        String[] expected = {"aba"};
+        Assert.assertEquals(actual, expected);
+    }
 
     @Test
     public void testRemoveConsecutiveDuplicates1() {
@@ -348,7 +355,7 @@ public class StringArrayUtilsTest {
     public void testRemoveConsecutiveDuplicates3() {
         String[] array = {"aba", "aba", "baa", "bab", "bba", "zzz", "bba", "bba", "bba", "bbb", "bbb", "aba", "bbb"};
         String[] actual = StringArrayUtils.removeConsecutiveDuplicates(array);
-        String[] expected = {"aba", "baa", "bab", "bba", "zzz", "bba", "aba", "bbb"};
+        String[] expected = {"aba", "baa", "bab", "bba", "zzz", "bba", "bbb", "aba", "bbb"};
         Assert.assertEquals(actual, expected);
     }
 
@@ -365,7 +372,7 @@ public class StringArrayUtilsTest {
     @Test
     public void testRemovePackDuplicates1() {
         String[] array = {"a", "a", "a", "a", "b", "c", "c", "a", "a", "d"};
-        String[] expected = {"aaa", "b", "cc", "aa", "d", "eee"};
+        String[] expected = {"aaaa", "b", "cc", "aa", "d"};
         String[] actual = StringArrayUtils.packConsecutiveDuplicates(array);
         Assert.assertEquals(expected, actual);
     }
@@ -375,7 +382,7 @@ public class StringArrayUtilsTest {
     @Test
     public void testRemovePackDuplicates2() {
         String[] array = {"t", "t", "q", "a", "a", "a", "b", "c", "c", "a", "a", "d", "e", "e", "e"};
-        String[] expected = {"tt", "q", "aaa", "cc", "aa", "d", "eee"};
+        String[] expected = {"tt", "q", "aaa", "b", "cc", "aa", "d", "eee"};
         String[] actual = StringArrayUtils.packConsecutiveDuplicates(array);
         Assert.assertEquals(expected, actual);
     }
@@ -390,6 +397,14 @@ public class StringArrayUtilsTest {
         Assert.assertEquals(expected, actual);
     }
 
+    @Test
+    public void testRemovePackDuplicates4() {
+        String[] array = {"a", "c", "b", "b"};
+        String[] expected = {"a","c", "bb"};
+        String[] actual = StringArrayUtils.packConsecutiveDuplicates(array);
+        Assert.assertEquals(expected, actual);
+
+    }
 
 
 
@@ -399,7 +414,7 @@ public class StringArrayUtilsTest {
 
     @Test
     public void testRemoveValue() {
-        String[] array = {"the", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"};
+        String[] array = {"The", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"};
         String[] expected = {"quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"};
         String[] actual = StringArrayUtils.removeValue(array, "The");
         Assert.assertEquals(expected, actual);
