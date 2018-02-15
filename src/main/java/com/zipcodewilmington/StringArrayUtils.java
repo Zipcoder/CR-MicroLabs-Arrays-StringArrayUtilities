b037c8491ace6fbab63ae4e9385d6f48882cef3d
Dylan Camac <dylancamac@zipcoders-MacBook-Pro-5.local>
2018-02-15 15:02:25 -0500

    kapow

--- src/main/java/com/zipcodewilmington/StringArrayUtils.java
+++ src/main/java/com/zipcodewilmington/StringArrayUtils.java
@@ -1,5 +1,7 @@
 package com.zipcodewilmington;
 
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/29/18.
  */
@@ -90,8 +92,12 @@ public class StringArrayUtils {
      */ // TODO
     public static boolean isPalindromic(String[] array) {
 
-        return true;
-
+        String[] reversed = reverse(array);
+        boolean isPalindrome = false;
+        if (Arrays.equals(reversed, array)) {
+            isPalindrome = true;
+        }
+        return isPalindrome;
     }
 
 
@@ -104,16 +110,16 @@ public class StringArrayUtils {
         String littleCase = "";
 
         for (int i = 0; i < array.length; i++) {
-
+            // array[i] sets all indexes in array to lowercase (in this example)
             littleCase = littleCase + array[i].toLowerCase();
         }
 
         int totalLetters = 0;
-
+        // were loooping through the alphabet here
         for (char j = 'a'; j <= 'z'; j++) {
-
+            // making sure we see the letter
             if (littleCase.indexOf(j) >= 0)
-
+                // when we see the letter, increment up one.
                 totalLetters++;
         }
 
@@ -142,7 +148,6 @@ public class StringArrayUtils {
             }
 
 
-
         }
 
         return occurences;
@@ -156,29 +161,30 @@ public class StringArrayUtils {
      */ // TODO
     public static String[] removeValue(String[] array, String valueToRemove) {
 
-        int characters = 0;
-        int removals = 0;
+        // i have an array and i want to remove a value from it
 
+        String[] newArray = new String[0];
+
+        // looping through the length of the given array
         for (int i = 0; i < array.length; i++) {
 
-            if (array[i].equals(valueToRemove)) {
+            // if the looped array does not equal value to remove
+            if (array[i] != valueToRemove) {
+                // make a copy of newArray, with length + 1
+                newArray = Arrays.copyOf(newArray, newArray.length + 1);
+                // we're accessing the new array minus length 1
+                // and setting it to the original array with the values we're looping through
+                newArray[newArray.length - 1] = array[i];
 
-                characters++;
             }
 
 
         }
 
-        String[] array2 = new String[array.length - characters];
 
-        for (int j = 0; j < array.length; j++) {
-
-            //stumped here
-
-            }
+        return newArray;
 
 
-            return null;
     }
 
     /**
@@ -186,7 +192,31 @@ public class StringArrayUtils {
      * @return array of Strings with consecutive duplicates removes
      */ // TODO
     public static String[] removeConsecutiveDuplicates(String[] array) {
-        return null;
+
+        // we want to remove one duplicate when it's next to it's "brother"
+        String[] answer = new String[1];
+
+        // first value keeps getting choppped, so we're putting it there to start.
+        answer[0] = array[0];
+
+        // we're looping thru the length of the array
+        for (int i = 1; i < array.length; i++) {
+
+            // if the array we're looping through is equal to itself - 1
+            if (!array[i].equals(array[i - 1])) {
+                // making copy of array + 1
+
+                answer = Arrays.copyOf(answer, answer.length + 1);
+                // setting new space to value of original
+                answer[answer.length - 1] = array[i];
+            }
+
+        }
+
+
+        return answer;
+
+
     }
 
     /**
@@ -194,7 +224,27 @@ public class StringArrayUtils {
      * @return array of Strings with each consecutive duplicate occurrence concatenated as a single string in an array of Strings
      */ // TODO
     public static String[] packConsecutiveDuplicates(String[] array) {
-        return null;
+        // check for duplicates first
+
+        StringBuilder myString = new StringBuilder();
+        myString.append(array[0]);
+
+
+        for (int i = 1; i < array.length; i++) {
+
+            // taking the array we're loooping thru and checking if the value before it is equal
+            if (array[i].equals(array[i - 1])) {
+
+                myString.append(array[i]);
+            } else {
+                myString.append(",");
+                myString.append(array[i]);
+            }
+
+        }
+        String newString = myString.toString();
+        return newString.split(",");
+
     }
 
 
