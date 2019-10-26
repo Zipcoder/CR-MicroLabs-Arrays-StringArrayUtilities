842b113b7988719b802491818a9f93e3c6b0486f
SandyPham823 <sandydupham@gmail.com>
2019-10-25 20:05:20 -0400

    Finished lab

--- src/main/java/com/zipcodewilmington/StringArrayUtils.java
+++ src/main/java/com/zipcodewilmington/StringArrayUtils.java
@@ -1,5 +1,9 @@
 package com.zipcodewilmington;
 
+import jdk.nashorn.internal.runtime.arrays.ArrayLikeIterator;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
@@ -83,25 +87,57 @@ public class StringArrayUtils {
          * @return true if each letter in the alphabet has been used in the array
          */ // TODO
         public static boolean isPangramic (String[]array){
-            return false;
-        }
+           String str = "";
+           boolean[] alphabetList = new boolean[26];
+           int index = 0;
+           int counter = 0;
+           for(int i = 0; i < array.length; i++){
+               str+= array[i];
+           }
+           str = str.replace(" ", "");
 
+           for (int i = 0; i < str.length(); i++){
+               if (str.charAt(i) >= 'A' && str.charAt(i) <= 'Z')
+                   index = str.charAt(i) - 'A';
+               else if (str.charAt(i) >= 'a' && str.charAt(i) <= 'z')
+                   index = str.charAt(i) - 'a';
+
+               alphabetList[index] = true;
+
+        }
+           for (int i = 0; i<= 25; i++){
+               if (!alphabetList[i])
+                   return false;
+               }
+           return true;
+           }
         /**
          * @param array array of String objects
          * @param value value to check array for
          * @return number of occurrences the specified `value` has occurred
          */ // TODO
         public static int getNumberOfOccurrences (String[]array, String value){
-            return 0;
+            int counter = 0;
+            for (int i = 0; i < array.length; i++){
+                if (array[i].equals(value)) {
+                counter++;
+                }
+            }
+            return counter;
         }
-
         /**
          * @param array         array of String objects
          * @param valueToRemove value to remove from array
          * @return array with identical contents excluding values of `value`
          */ // TODO
         public static String[] removeValue (String[]array, String valueToRemove){
-            return null;
+            ArrayList<String> arrayNoRepeats = new ArrayList<String>();
+            for (int i = 0; i < array.length; i++) {
+                if (!array[i].equals(valueToRemove)) {
+                    arrayNoRepeats.add(array[i]);
+                }
+            }
+            return arrayNoRepeats.toArray(new String[arrayNoRepeats.size()]);
         }
 
         /**
@@ -109,7 +145,17 @@ public class StringArrayUtils {
          * @return array of Strings with consecutive duplicates removes
          */ // TODO
         public static String[] removeConsecutiveDuplicates (String[]array){
-            return null;
+            ArrayList<String> arrayNoRepeats = new ArrayList<String>();
+            for (int i = 0; i < array.length; i++) {
+                if (array.length - 1 == i) {
+                    arrayNoRepeats.add(array[i]);
+                }
+                else if (!array[i].equals(array[i + 1])) {
+                    arrayNoRepeats.add(array[i]);
+                }
+
+            }
+            return arrayNoRepeats.toArray(new String[arrayNoRepeats.size()]);
         }
 
         /**
@@ -117,7 +163,20 @@ public class StringArrayUtils {
          * @return array of Strings with each consecutive duplicate occurrence concatenated as a single string in an array of Strings
          */ // TODO
         public static String[] packConsecutiveDuplicates (String[]array){
-            return null;
+            ArrayList<String> arrayPack = new ArrayList<String>();
+            arrayPack.add(array[0]);
+            int j = 0;
+            for (int i = 1; i < array.length; i++) {
+                if (array[i].equals(Character.toString(arrayPack.get(j).charAt(0)))) {
+                String concated = arrayPack.get(j)+array[i];
+                arrayPack.set(j, concated);
+                }
+                else{
+                    arrayPack.add(array[i]);
+                    j++;
+                }
+            }
+            return arrayPack.toArray(new String[arrayPack.size()]);
         }
 
 
